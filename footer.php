5e3d89b3f8ce477ebb223f43752b30d4a19fec51
deepak <deepakbaradwaj933@gmail.com>
2023-08-08 13:01:42 +0530

    a

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
 <footer>
     <div class="row">
         <div class="col-4">
-            <div class="footerlogo"><a href="<?= base_url() ?>"><img src="<?= base_url() ?>assets/images/logo.png" alt=""></a></div>
+            <div class="footerlogo"><a href="<?= get_home_url() ?>"><img src="<?= get_home_url() ?>assets/images/logo.png" alt=""></a></div>
             <!-- <p>&copy; 2023 All rights reserved.</p> -->
         </div>
         <div class="col-4">
             <ul>
-                <li><a href="<?= base_url() ?>terms-of-services">Terms of Services</a></li>
-                <li><a href="<?= base_url() ?>privacy-policy">Privacy Policy</a></li>
-                <li><a href="<?= base_url() ?>contact-us">Contact</a></li>
+                <li><a href="<?= get_home_url() ?>terms-of-services">Terms of Services</a></li>
+                <li><a href="<?= get_home_url() ?>privacy-policy">Privacy Policy</a></li>
+                <li><a href="<?= get_home_url() ?>contact-us">Contact</a></li>
             </ul>
         </div>
         <div class="col-4 follow_icon">
