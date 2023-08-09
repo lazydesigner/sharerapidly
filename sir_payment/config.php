8c633f364cdc9a1ee7df742ca43f56105289f8fc
deepak <deepakbaradwaj933@gmail.com>
2023-08-09 16:34:33 +0530

    payment button

---
+++ sir_payment/config.php
@@ -0,0 +1,18 @@
+<?php 
+/* 
+ * PayPal and database configuration 
+ */ 
+  
+// PayPal configuration 
+define('PAYPAL_ID', 'info@pksolutions.org'); 
+define('PAYPAL_SANDBOX', FALSE); //TRUE or FALSE 
+ 
+define('PAYPAL_RETURN_URL', ''. base_url() .'success.php'); 
+define('PAYPAL_CANCEL_URL', ''. base_url() .'cancel.php'); 
+define('PAYPAL_NOTIFY_URL', ''. base_url() .'ipn.php'); 
+define('PAYPAL_CURRENCY', 'USD'); 
+ 
+
+ 
+// Change not required 
+define('PAYPAL_URL', (PAYPAL_SANDBOX == true) ? "https://www.sandbox.paypal.com/cgi-bin/webscr" : "https://www.paypal.com/cgi-bin/webscr");
